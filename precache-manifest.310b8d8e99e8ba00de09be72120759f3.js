9b1462bd3f759e56698ebeeeb4ac7e869f481e35
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-03 01:37:18 +0000

    Deploy marunyak/js-band-hw-task-6 to github.com/marunyak/js-band-hw-task-6.git:gh-pages

---
+++ precache-manifest.310b8d8e99e8ba00de09be72120759f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17efaa1fc13df0cb969b3a6a0687b32b",
+    "url": "/js-band-hw-task-6/index.html"
+  },
+  {
+    "revision": "31f56616ad458585f1fc",
+    "url": "/js-band-hw-task-6/static/js/2.eb311fb1.chunk.js"
+  },
+  {
+    "revision": "247dadb49cf5756769fa",
+    "url": "/js-band-hw-task-6/static/js/main.795fd6f3.chunk.js"
+  },
+  {
+    "revision": "352bb33da0248432e729",
+    "url": "/js-band-hw-task-6/static/js/runtime-main.b55a36ec.js"
+  }
+]);
